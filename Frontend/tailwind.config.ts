a8b72fd3330006a2b9a45afeb4d128bbde0f57bb
YsisC <ysisclongart@gmail.com>
2023-11-28 14:16:25 -0300

    adding font-family poppins

--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -33,6 +33,9 @@ const config: Config = {
 
     },
     extend: {
+      fontFamily: {
+        sans: ['Poppins', ],
+     },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -42,4 +45,4 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
+
