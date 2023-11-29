b3d10d9a4b64da011e5833471e6f6ab4b9a54214
arbelaism <arbelaism@outlook.com>
2023-11-29 17:40:22 -0300

    style: improve counter button

--- Frontend/src/components/Counter/index.tsx
+++ Frontend/src/components/Counter/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useCounterStore } from "@/store/zustand";
 
 export default function Counter() {
@@ -7,13 +8,21 @@ export default function Counter() {
 
 	// FIX: add colors to tailwind config
 	return (
-		<div className="w-full py-5 px-3 font-semibold shadow-2xl bg-[#EBEBEB] text-[#1A1A1A] rounded-3xl text-base flex items-center gap-2 justify-around">
-			<button type="button" onClick={decrement}>
-				-
+		<div className="w-max p-2 text-2xl gap-8 font-semibold font-montserrat shadow-2xl bg-[#EBEBEB] text-[#1A1A1A] rounded-3xl flex items-center gap-2 justify-around">
+			<button
+				type="button"
+				className="w-8 h-8 text-center text-3xl flex items-center justify-center text-white font-bold rounded-full"
+				onClick={decrement}
+			>
+				<Image width={32} height={32} src={"/minusIcon.svg"} alt="minus icon" />
 			</button>
 			<span>{count}</span>
-			<button type="button" onClick={increment}>
-				+
+			<button
+				type="button"
+				className="w-8 h-8 flex text-3xl items-center justify-center text-white font-bold rounded-full"
+				onClick={increment}
+			>
+				<Image width={32} height={32} src={"/plusIcon.svg"} alt="minus icon" />
 			</button>
 		</div>
 	);
