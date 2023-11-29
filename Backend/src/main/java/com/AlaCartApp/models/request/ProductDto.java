60997ae34eec0a1bb495d258aa6110a37ebe5de7
GROblivium <lgreyesmiranda@gmail.com>
2023-11-29 20:57:18 -0300

    Fix Creación de mapper y dto de Product, category, subcategory, Entity, User

--- Backend/src/main/java/com/AlaCartApp/models/request/ProductDto.java
+++ Backend/src/main/java/com/AlaCartApp/models/request/ProductDto.java
@@ -2,24 +2,15 @@ package com.AlaCartApp.models.request;
 
 import com.AlaCartApp.models.entity.Category;
 import com.AlaCartApp.models.entity.Image;
-import lombok.Getter;
-import lombok.Setter;
-
+import lombok.Data;
 import java.util.List;
 
-@Getter @Setter
+@Data
 public class ProductDto {
-
     Long id;
-
     String name;
-
     Double price;
-
     Category category;
-
     String description;
-
     List<Image> images;
-
 }
