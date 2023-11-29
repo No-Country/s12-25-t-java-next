0f26ac56ad1fc024b2be771476f7bf0b477a3ef5
arbelais <arbelaism@outlook.com>
2023-11-29 16:10:58 -0300

    refactor: add index.ts for organize imports/exports

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
-import Notification from "@/components/Modal/Notification";
-import Image from "next/image";
 import DishesPage from "./[slug]/page";
+import { Button, Counter, Notification } from "@/components";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-[100vh] w-screen flex-col bg-whitebackground items-center justify-between overflow-hidden">
-      <DishesPage />
-      <Notification />
-    </main>
-  );
+	return (
+		<main className="flex min-h-[100vh] w-screen flex-col bg-whitebackground items-center justify-between overflow-hidden">
+			<DishesPage />
+			<Notification />
+			<Counter />
+		</main>
+	);
 }
