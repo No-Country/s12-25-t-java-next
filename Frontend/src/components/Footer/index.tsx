b7512edba043754e4813b590c3a63d86d519e80d
YsisC <ysisclongart@gmail.com>
2023-11-29 17:50:42 -0300

    merge components footer with callWaiter

--- Frontend/src/components/Footer/index.tsx
+++ Frontend/src/components/Footer/index.tsx
@@ -5,17 +5,8 @@ import { CallWaiterPopup } from "../Popup/Popup";
 const Footer = () => {
   return (
     <footer className="sticky bottom-[0] z-[99] w-screen px-4 py-6 bg-white shadow-footer overflow-hidden">
-      <CallWaiterPopup />
       <div className="flex justify-between xl:px-4">
-        <button className="bg-secondary-100 p-3 rounded-full shadow-button flex items-center justify-center">
-        
-        
-          <Image
-           src={"/Call.svg"}
-            height={30}
-             width={30} 
-             alt="call waiter" />
-        </button>
+      <CallWaiterPopup />
         <button className="bg-primary-100 p-3 rounded-full shadow-button">
           <Image
             src={"/Carrito.svg"}
