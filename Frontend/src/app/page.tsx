686e20206766a57138980a9784843aa1ffed8223
arbelaism <arbelaism@outlook.com>
2023-11-30 21:27:20 -0300

    fix: fix routes temporarily

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
-import { products } from "@/utils/data";
-import { Notification, ProductDescription } from "@/components";
-
+// import { products } from "@/utils/data";
+// import { Notification, ProductDescription } from "@/components";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-[100vh] w-screen flex-col bg-whitebackground items-center justify-between">
-      <Notification />
-    </main>
-  );
+	// TODO: create home page if its needed
+	redirect("/menu");
+	// return (
+	// 	<main className="flex min-h-[100vh] w-screen flex-col bg-whitebackground items-center justify-between">
+	// 		<Notification />
+	// 	</main>
+	// );
 }
