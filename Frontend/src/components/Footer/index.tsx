f697cd714961de813a54305f7eb653211ccfef9a
YsisC <ysisclongart@gmail.com>
2023-12-01 18:09:54 -0300

    Style from botom header

--- Frontend/src/components/Footer/index.tsx
+++ Frontend/src/components/Footer/index.tsx
@@ -10,14 +10,16 @@ const Footer = () => {
         <CallWaiterPopup />
         <button
           type="button"
-          className="bg-primary-100 p-3 rounded-full shadow-button"
+          className="bg-primary-100 px-3 w-[10.5rem] py-4 font-medium text-sm text-white rounded-[1.3rem]  shadow-button flex items-center justify-between"
         >
           <Image
             src={"/Carrito.svg"}
             height={30}
             width={30}
             alt="add to cart"
+            className="pr-1"
           />
+          Carrito
         </button>
       </div>
       <Divider />
