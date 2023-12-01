f62277676750da89f095efb95747555d16f023f9
YsisC <ysisclongart@gmail.com>
2023-12-01 18:22:37 -0300

    Link cart

--- Frontend/src/components/Footer/index.tsx
+++ Frontend/src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { CallWaiterPopup } from "../Popup/Popup";
+import Link from "next/link";
 import Divider from "./Divider";
 
 const Footer = () => {
@@ -8,6 +9,12 @@ const Footer = () => {
     <footer className="sticky bottom-[0] z-[99] w-screen px-4 py-6 bg-white shadow-footer overflow-hidden">
       <div className="flex justify-between xl:px-4">
         <CallWaiterPopup />
+        <Link
+        passHref
+      href={'cart'} >
+
+     
+    
         <button
           type="button"
           className="bg-primary-100 px-3 w-[10.5rem] py-4 font-medium text-sm text-white rounded-[1.3rem]  shadow-button flex items-center justify-center "
@@ -21,6 +28,7 @@ const Footer = () => {
           />
           Carrito
         </button>
+        </Link>
       </div>
       <Divider />
     </footer>
