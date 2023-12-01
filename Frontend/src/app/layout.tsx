73666fdfb8f3ab9f91871acc4da4b29e0ac6ba10
arbelaism <arbelaism@outlook.com>
2023-11-30 22:53:25 -0300

    fix: redirect everything to dishes temporarily

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,28 +1,22 @@
 import type { Metadata } from "next";
 import { montserrat } from "./fonts";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: {
-    default: "AlaCartApp",
-    template: "%s | AlaCartApp",
-  },
+	title: {
+		default: "AlaCartApp",
+		template: "%s | AlaCartApp",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="es" className={`${montserrat.variable}`}>
-      <body suppressHydrationWarning={true}>
-    
-        {children}
-       
-      </body>
-    </html>
-  );
+	return (
+		<html lang="es" className={`${montserrat.variable}`}>
+			<body suppressHydrationWarning={true}>{children}</body>
+		</html>
+	);
 }
--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
-import { products } from "@/utils/data";
-import { Notification, ProductDescription } from "@/components";
-
+// import { products } from "@/utils/data";
+// import { Notification, ProductDescription } from "@/components";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-[100vh] w-screen flex-col bg-whitebackground items-center justify-between">
-      <Notification />
-    </main>
-  );
+	redirect("/dishes");
+	// return (
+	//   <main className="flex min-h-[100vh] w-screen flex-col bg-whitebackground items-center justify-between">
+	//     <Notification />
+	//   </main>
+	// );
 }
