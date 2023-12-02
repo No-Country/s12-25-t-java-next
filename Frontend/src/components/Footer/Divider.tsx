6c751169207f4466f9cb6346f1e564303685f526
John Pablo Valverde <80083262+johnpablo7@users.noreply.github.com>
2023-12-02 13:15:29 -0500

    fix

--- Frontend/src/components/Footer/Divider.tsx
+++ Frontend/src/components/Footer/Divider.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { DeleteOrdersPopup } from "../Popup/DeleteOrdersPopup";
 
 const Divider = () => {
   return (
     <div className="flex items-center justify-center pt-2">
       <div className="bg-black w-[72px] h-[2px] rounded-lg" />
-      <DeleteOrdersPopup />
     </div>
   );
 };
