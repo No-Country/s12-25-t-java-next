bc6a426cf743e9846beabd1b20aabb9cddba578b
Torneyus <jjd21017@gmail.com>
2023-12-04 09:00:17 -0500

    Order Controller

---
+++ Backend/src/main/java/com/AlaCartApp/controller/OrderController.java
@@ -0,0 +1,55 @@
+package com.AlaCartApp.controller;
+
+import com.AlaCartApp.models.entity.Order;
+import com.AlaCartApp.service.abstraction.OrderService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrderWithDetails(@RequestBody Order order) {
+        return ResponseEntity.ok(orderService.create(order));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> ListOrder() {
+        return ResponseEntity.ok(orderService.findAll());
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<Order> updateOrder(@PathVariable Long id, @RequestBody Order order) {
+        return ResponseEntity.ok(orderService.update(id, order));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> deleteOrder(@PathVariable Long id) {
+        orderService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Order> returnOrder(@PathVariable Long id){
+        return ResponseEntity.ok(orderService.find(id));
+    }
+}
---
+++ Backend/src/main/java/com/AlaCartApp/service/abstraction/OrderService.java
@@ -0,0 +1,18 @@
+package com.AlaCartApp.service.abstraction;
+
+import com.AlaCartApp.models.entity.Order;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface OrderService {
+    List<Order> findAll();
+
+    Order create(Order order);
+
+    Order update(Long id, Order order);
+
+    void delete(Long id);
+
+    Order find(Long id);
+}
