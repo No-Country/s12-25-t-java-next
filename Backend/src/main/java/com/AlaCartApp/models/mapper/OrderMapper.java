b8366b5ef9ed971c135a7bcfe96a365800816743
Torneyus <jjd21017@gmail.com>
2023-12-04 14:51:36 -0500

    OrderDto

---
+++ Backend/src/main/java/com/AlaCartApp/models/mapper/OrderMapper.java
@@ -0,0 +1,28 @@
+package com.AlaCartApp.models.mapper;
+
+import com.AlaCartApp.models.entity.Order;
+import com.AlaCartApp.models.request.OrderDto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+
+    @Mappings({
+            @Mapping(source = "id", target = "id"),
+            @Mapping(source = "table", target = "table"),
+            @Mapping(source = "detail", target = "detail"),
+            @Mapping(source = "paymentMethod", target = "paymentMethod"),
+            @Mapping(source = "total", target = "total"),
+            @Mapping(source = "state", target = "state")
+    })
+    OrderDto toOrderDTO(Order order);
+    List<OrderDto> toOrdersDTO(List<Order> order);
+    @InheritInverseConfiguration
+    Order toOrder(OrderDto orderDto);
+
+}
---
+++ Backend/src/main/java/com/AlaCartApp/models/request/OrderDto.java
@@ -0,0 +1,23 @@
+package com.AlaCartApp.models.request;
+
+import com.AlaCartApp.enums.State;
+import com.AlaCartApp.models.entity.OrderDetail;
+import com.AlaCartApp.models.entity.TableEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderDto {
+    private Long id;
+    private TableEntity tableEntity;
+    private LocalDateTime date;
+    private LocalDateTime updated;
+    private List<OrderDetail> detail;
+    private String paymentMethod;
+    private Double total;
+    private State state;
+}
