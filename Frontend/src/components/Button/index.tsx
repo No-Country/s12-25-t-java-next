b642f56725a948310a865ba9298fe3e040913c1f
arbelaism <arbelaism@outlook.com>
2023-12-03 21:51:49 -0300

    refactor!: add variants and remove logic

--- Frontend/src/components/Button/index.tsx
+++ Frontend/src/components/Button/index.tsx
@@ -1,20 +1,28 @@
-import { Product } from "../../types/Product";
-export default function Button({
-  text,
-  handleAddToCart,
-  product,
-}: {
+import clsx from "clsx";
+import { ButtonHTMLAttributes } from "react";
+
+interface Props {
   text: string;
-  handleAddToCart: (product: Product) => void;
-  product: Product;
-}) {
+  variant: "primary" | "secondary";
+  props?: ButtonHTMLAttributes<HTMLButtonElement>;
+}
+
+const BUTTON_VARIANTS = {
+  primary: "bg-primary-100 text-white",
+  secondary: "bg-secondary-100 text-white",
+};
+
+export default function Button({ text, variant, props }: Props) {
   return (
     <button
       type="button"
-      className="w-full md:w-max h-full p-2 md:py-5 md:px-6 bg-primary-100 rounded-3xl text-white shadow-2xl"
-      onClick={() => handleAddToCart(product)}
+      className={clsx(
+        BUTTON_VARIANTS[variant],
+        "w-full md:w-max h-max p-2 md:py-5 md:px-6 rounded-3xl shadow-2xl",
+      )}
+      {...props}
     >
       {text}
     </button>
   );
-  }
+}
