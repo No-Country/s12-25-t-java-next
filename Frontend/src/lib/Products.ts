9b2734a5c5b2c233b7a7a5dec91555cd7a222b21
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-12-04 22:58:41 -0400

    installed sharp for image optimization on production / deleted loader component on RootLayout / no-cache on products data fetching / loading UI fallback component

--- Frontend/src/lib/Products.ts
+++ Frontend/src/lib/Products.ts
@@ -1,17 +1,16 @@
-import "server-only";
+'server only'
+import { Product } from '@/types/Product'
 
-import { Product } from "@/types/Product";
-
-const baseUrl = process.env.BASE_URL;
+const baseUrl = process.env.BASE_URL
 
 export async function getProducts(): Promise<Product[]> {
-	const url = `${baseUrl}/api/products`;
-	const res = await fetch(url);
-	return await res.json();
+	const url = `${baseUrl}/api/products`
+	const res = await fetch(url, { cache: 'no-cache' })
+	return await res.json()
 }
 
 export async function getProductById(id: string): Promise<Product> {
-	const url = `${baseUrl}/api/products/${id}`;
-	const res = await fetch(url);
-	return await res.json();
+	const url = `${baseUrl}/api/products/${id}`
+	const res = await fetch(url)
+	return await res.json()
 }
