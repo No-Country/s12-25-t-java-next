f5688a1e805b9d3e1dee78dcf2c4adbf5045f130
YsisC <ysisclongart@gmail.com>
2023-12-05 16:26:01 -0300

    Style notification and button

--- Frontend/src/app/(cart)/cart/page.tsx
+++ Frontend/src/app/(cart)/cart/page.tsx
@@ -31,10 +31,10 @@ const CartPage = () => {
       <SummaryCart />
       <footer className=" fixed bottom-0  px-4 py-3 w-screen">
         <div className="flex justify-between items-center">
-          <button type="button" className="border-2 w-[10.3rem] h-[2.5rem] text-[1rem] border-primary-100 px-5 py-2 rounded-[1.3rem]">
+          <button type="button" className="border-2 w-[10.3rem] h-[2.5rem] text-[0.75rem] border-primary-100 px-5 py-2 font-medium rounded-[1.3rem]">
             Ver Ticket
           </button>
-          <button type="button" className="border-none w-[10.3rem] h-[2.5rem] text-[1rem] bg-primary-100 hover:bg-primary-200 px-5 py-2 rounded-[1.3rem]">
+          <button type="button" className="border-none w-[10.3rem] h-[2.5rem] text-[0.75rem] ml-1 bg-primary-100 hover:bg-primary-200 font-medium px-5 py-2 rounded-[1.3rem]">
             Realizar pedido
           </button>
         </div>
--- Frontend/src/components/Modal/Notification.tsx
+++ Frontend/src/components/Modal/Notification.tsx
@@ -11,7 +11,7 @@ const Notification = () => {
     <div
       className={
         showMessageBoolean && message
-          ? "fixed z-[100] bg-secondary-100 w-[100vw] md:w-[24rem] h-28 rounded-b-lg text-white  top-[0%] md:right-[0%] px-4  flex items-center justify-between ease-in-out duration-500"
+          ? "fixed z-[100] bg-secondary-100 w-[100vw] md:w-[24rem] h-28 rounded-b-lg text-white  top-[0%] right-[0%] px-4  flex items-center justify-between ease-in-out duration-500"
           : "fixed top-[-100%] "
       }
     >
@@ -19,12 +19,12 @@ const Notification = () => {
         <>
           <Image
             src={message.svg}
-            width={25}
-            height={25}
+            width={8} 
+            height={8}
             alt="notification waiter"
-            className="object-cover w-[6.5rem] h-full pt-1"
+            className=" w-[27%] h-[112px] pt-4"
           />
-          <h3 className="font-sans"> {message.text}</h3>{" "}
+          <h3 className="font-sans text-[1rem] ml-2"> {message.text}</h3>{" "}
         </>
       )}
     </div>
