6f0df13ca2ef1a08bad119c2e4ffede534cc81b8
YsisC <ysisclongart@gmail.com>
2023-12-06 16:40:07 -0300

    order example

--- Frontend/src/app/(cart)/cart/page.tsx
+++ Frontend/src/app/(cart)/cart/page.tsx
@@ -13,9 +13,14 @@ import HeaderBack from "@/components/Header/HeaderBack";
 import { useNotifyStore } from "@/store/zustand";
 
 const CartPage = () => {
-  const { cart, removeAll } = useCartStore();
+  const { cart, removeAll, subtotal } = useCartStore();
   const router = useRouter();
   const { add, setShowMessageBoolean } = useNotifyStore();
+  useEffect(() => {
+    subtotal();
+  }, [cart, subtotal]);
+  // Call subtotal once and store the result in a variable
+  const cartSubtotal = subtotal();
 
   const handleNotification = () => {
     setTimeout(() => {
@@ -23,16 +28,25 @@ const CartPage = () => {
     }, 2500);
   };
   const handleOrder = () => {
-    console.log('order')
+    const orderData = {
+      table: 1,
+      date: new Date(),
+      state: "pending",
+      subTotal: cartSubtotal,
+      payment:'inProcees',
+      products: cart,
+    };
+    console.log("order", orderData);
     const newMessage = {
-      text:
-      (
+      text: (
         <>
-        <p>La orden ha sido confirmada.<br/>
-        Tu pedido está en camino.</p>
-      </>
-      ) ,
-
+          <p>
+            La orden ha sido confirmada.
+            <br />
+            Tu pedido está en camino.
+          </p>
+        </>
+      ),
 
       svg: "/icon/Group 8.svg",
     };
@@ -58,12 +72,17 @@ const CartPage = () => {
       <SummaryCart />
       <footer className=" fixed bottom-0  px-4 py-3 w-screen">
         <div className="flex justify-between items-center">
-          <button type="button" className="border-2 w-[10.3rem] h-[2.5rem] text-[0.75rem] border-primary-100 px-5 py-2 font-medium rounded-[1.3rem]">
+          <button
+            type="button"
+            className="border-2 w-[10.3rem] h-[2.5rem] text-[0.75rem] border-primary-100 px-5 py-2 font-medium rounded-[1.3rem]"
+          >
             Ver Ticket
           </button>
           <button
-           onClick={handleOrder} 
-           type="button" className="border-none w-[10.3rem] h-[2.5rem] text-[0.75rem] ml-1 bg-primary-100 hover:bg-primary-200 font-medium px-5 py-2 rounded-[1.3rem]">
+            onClick={handleOrder}
+            type="button"
+            className="border-none w-[10.3rem] h-[2.5rem] text-[0.75rem] ml-1 bg-primary-100 hover:bg-primary-200 font-medium px-5 py-2 rounded-[1.3rem]"
+          >
             Realizar pedido
           </button>
         </div>
