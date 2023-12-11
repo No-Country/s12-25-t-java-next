76dddeefb1e77bae95008110536b4582dcd3d8e9
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-12-11 10:57:25 -0400

    relocation of title template to the rootlayout of the app

--- Frontend/src/app/[slug]/page.tsx
+++ Frontend/src/app/[slug]/page.tsx
@@ -1,71 +1,74 @@
-import FilterSelected from "@/components/FilterProducts/filterSelected";
-import ProductsList from "@/components/Products/ProductsList";
-import { Product } from "@/types/Product";
-import { productsAndSubcats } from "@/utils/productBreakdown";
-import { filterProducts, filterProductsBySubcategory } from "@/utils/searchLogic/filterBySubcategory";
-import React from "react";
-import { Suspense } from "react";
-import unorm from "unorm";
+import FilterSelected from '@/components/FilterProducts/filterSelected'
+import ProductsList from '@/components/Products/ProductsList'
+import { Product } from '@/types/Product'
+import { productsAndSubcats } from '@/utils/productBreakdown'
+import {
+	filterProducts,
+	filterProductsBySubcategory,
+} from '@/utils/searchLogic/filterBySubcategory'
+import { Metadata } from 'next'
+import React from 'react'
+import unorm from 'unorm'
 
-export const metadata = {
-  title: "Platos",
-};
+export const metadata: Metadata = {
+	title: 'Menú',
+}
 interface MenuPageProps {
-  params: { slug: string };
-  searchParams?: { query: string | undefined , sort: string | undefined }
+	params: { slug: string }
+	searchParams?: { query: string | undefined; sort: string | undefined }
 }
 
+async function MenuPage({ params, searchParams }: MenuPageProps) {
+	const { productsByCategory, subcategories } = await productsAndSubcats(
+		params.slug,
+		searchParams?.query
+	)
 
-async function MenuPage({
-  params,
-  searchParams,
-}: MenuPageProps) {
-  const { productsByCategory, subcategories } = await productsAndSubcats(
-    params.slug,
-    searchParams?.query
-  );
-  
-  const productsBySubcategories: Product[][] = subcategories.map((subcategory) => {
-    if (searchParams?.sort && !searchParams.sort.includes(subcategory)) {
-      return [];}
-    
-    const filteredProductsBySubcategory = filterProductsBySubcategory(
-      subcategory,
-      productsByCategory,
-      searchParams
-    );
-    const filteredProducts = filterProducts(productsByCategory, searchParams);
-  
-    return filteredProducts.length > 0 ? filteredProducts : filteredProductsBySubcategory;
-  });
-  
-  const hasProducts = productsBySubcategories.some(
-    (products) => products.length > 0
-  );
-  
-console.log(productsBySubcategories)
-  return (
-    <>
-      <FilterSelected />
-      <Suspense fallback={<p>Carajo...</p>}>
-        {hasProducts ? (
-          productsBySubcategories.map((products, index) => (
-            <div key={subcategories[index]} className="pl-5">
+	const productsBySubcategories: Product[][] = subcategories.map(
+		(subcategory) => {
+			if (searchParams?.sort && !searchParams.sort.includes(subcategory)) {
+				return []
+			}
 
-              <ProductsList
-                products={products}
-                listCarousel={subcategories.length > 1 ? true : false}
-              />
-            </div>
-          ))
-        ) : (
-          <p className="text-center">
-            No se ha encontrado “{searchParams?.query}” en esta sección.
-          </p>
-        )}
-      </Suspense>
-      )
-    </>
-  );
+			const filteredProductsBySubcategory = filterProductsBySubcategory(
+				subcategory,
+				productsByCategory,
+				searchParams
+			)
+			const filteredProducts = filterProducts(productsByCategory, searchParams)
+
+			return filteredProducts.length > 0
+				? filteredProducts
+				: filteredProductsBySubcategory
+		}
+	)
+
+	const hasProducts = productsBySubcategories.some(
+		(products) => products.length > 0
+	)
+
+	console.log(productsBySubcategories)
+	return (
+		<>
+			<FilterSelected />
+			{hasProducts ? (
+				productsBySubcategories.map((products, index) => (
+					<div
+						key={subcategories[index]}
+						className="pl-5"
+					>
+						<ProductsList
+							products={products}
+							listCarousel={subcategories.length > 1 ? true : false}
+						/>
+					</div>
+				))
+			) : (
+				<p className="text-center">
+					No se ha encontrado “{searchParams?.query}” en esta sección.
+				</p>
+			)}
+		</>
+	)
 }
-export default MenuPage;
+export default MenuPage
