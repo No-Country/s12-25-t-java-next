2d3be77ed82a44266e60e8172699824d57bc7dd1
GROblivium <lgreyesmiranda@gmail.com>
2023-12-11 17:12:35 -0300

    Arreglo de DTO y Mapper de Order

--- Backend/src/main/java/com/AlaCartApp/models/mapper/OrderMapper.java
+++ Backend/src/main/java/com/AlaCartApp/models/mapper/OrderMapper.java
@@ -9,18 +9,16 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
+        @Mapping(source = "id" , target = "id")
+        @Mapping(source = "tableEntity" , target = "tableEntity")
+        @Mapping(source = "date", target = "date")
+        @Mapping(source = "detail", target = "detail")
+        @Mapping(source = "paymentMethod", target = "paymentMethod")
+        @Mapping(source = "total", target = "total")
+        @Mapping(source = "state", target = "state")
 
-//        @Mapping(source = "id" , target = "id")
-//        @Mapping(source = "tableEntity" , target = "tableEntity")
-//        @Mapping(source = "date", target = "date")
-//        @Mapping(source = "detail", target = "detail")
-//        @Mapping(source = "paymentMethod", target = "paymentMethod")
-//        @Mapping(source = "total", target = "total")
-//       // @Mapping(source = "state", target = "state")
-//
-//        OrderDto toOrderDTO (Order order);
-//        List<OrderDto> toOrdersDTO (List <Order> order);
-//        @InheritInverseConfiguration
-//
-//        Order toOrder (OrderDto orderDto);
+        OrderDto toOrderDTO (Order order);
+        List<OrderDto> toOrdersDTO (List <Order> order);
+        @InheritInverseConfiguration
+        Order toOrder (OrderDto orderDto);
 }
