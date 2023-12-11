79d4ae2695999c6112e33e21645a2d633ee59883
Ever Jose Rojas perez <everjosejr@hotmail.com>
2023-12-11 09:00:55 -0500

    fix- reorganize the ccode with the new data

--- Frontend/src/app/[slug]/page.tsx
+++ Frontend/src/app/[slug]/page.tsx
@@ -1,59 +1,49 @@
-
-import FilterSelected from '@/components/FilterProducts/filterSelected'
-import ProductsList from '@/components/Products/ProductsList'
-import { Product } from '@/types/Product'
-import { productsAndSubcats } from '@/utils/productBreakdown'
-import { Suspense } from 'react'
-import unorm from 'unorm';
-
-
+import FilterSelected from "@/components/FilterProducts/filterSelected";
+import ProductsList from "@/components/Products/ProductsList";
+import { Product } from "@/types/Product";
+import { productsAndSubcats } from "@/utils/productBreakdown";
+import { filterProducts, filterProductsBySubcategory } from "@/utils/searchLogic/filterBySubcategory";
+import React from "react";
+import { Suspense } from "react";
+import unorm from "unorm";
 
 export const metadata = {
   title: "Platos",
 };
+interface MenuPageProps {
+  params: { slug: string };
+  searchParams?: { query: string | undefined , sort: string | undefined }
+}
+
 
 async function MenuPage({
   params,
   searchParams,
-}: {
-  params: { slug: string };
-  searchParams?: { query: string };
-}) {
+}: MenuPageProps) {
   const { productsByCategory, subcategories } = await productsAndSubcats(
     params.slug,
     searchParams?.query
   );
-  const productsBySubcategories: Product[][] = subcategories.map(
-    (subcategory) => {
-      return !searchParams?.query
-        ? productsByCategory.filter(
-            (product) => product.subCategory.name === subcategory
-          )
-        : productsByCategory.filter((product) => {
-          const subcategoryNormalized = unorm.nfd(product.subCategory.name.toLowerCase()).replace(/[\u0300-\u036f]/g, '');
-  const searchSubcategoryNormalized = unorm.nfd(subcategory.toLowerCase()).replace(/[\u0300-\u036f]/g, '');
-
-  const subcategoryMatch = subcategoryNormalized === searchSubcategoryNormalized;
-  const sensitiveSearchParams = unorm.nfd(searchParams?.query.toLowerCase()).replace(/[\u0300-\u036f]/g, '');
-  const sensitiveProduct = unorm.nfd(product.name.toLowerCase()).replace(/[\u0300-\u036f]/g, '');
-  console.log(`Product Name: ${sensitiveProduct}, Query: ${sensitiveSearchParams}`);
-
-  const nameMatch = sensitiveProduct.includes(sensitiveSearchParams);
-  const descriptionMatch = product.description?.toLowerCase().includes(sensitiveSearchParams);
-
-  console.log(`${product.name}: Subcategory Match - ${subcategoryMatch}, Name Match - ${nameMatch}, Description Match - ${descriptionMatch}`);
-
-  return subcategoryMatch && (nameMatch || descriptionMatch);
-        });
-         
-    }
-  );
-  console.log(productsByCategory)
-
+  
+  const productsBySubcategories: Product[][] = subcategories.map((subcategory) => {
+    if (searchParams?.sort && !searchParams.sort.includes(subcategory)) {
+      return [];}
+    
+    const filteredProductsBySubcategory = filterProductsBySubcategory(
+      subcategory,
+      productsByCategory,
+      searchParams
+    );
+    const filteredProducts = filterProducts(productsByCategory, searchParams);
+  
+    return filteredProducts.length > 0 ? filteredProducts : filteredProductsBySubcategory;
+  });
+  
   const hasProducts = productsBySubcategories.some(
     (products) => products.length > 0
   );
- 
+  
+console.log(productsBySubcategories)
   return (
     <>
       <FilterSelected />
@@ -61,9 +51,11 @@ async function MenuPage({
         {hasProducts ? (
           productsBySubcategories.map((products, index) => (
             <div key={subcategories[index]} className="pl-5">
-              <ProductsList products={products} 
-			  	listCarousel={subcategories.length > 1 ? true : false}/>
-			  
+
+              <ProductsList
+                products={products}
+                listCarousel={subcategories.length > 1 ? true : false}
+              />
             </div>
           ))
         ) : (
