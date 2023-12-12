0826c84872075bb4d76b00cd33bfc64db17f56ff
Ra <108808330+Raul1978x@users.noreply.github.com>
2023-12-12 18:06:38 -0300

    Update CorsConfig.java

--- Backend/src/main/java/com/AlaCartApp/config/CorsConfig.java
+++ Backend/src/main/java/com/AlaCartApp/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("https://alacartapp-production.up.railway.app/**")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(false);
