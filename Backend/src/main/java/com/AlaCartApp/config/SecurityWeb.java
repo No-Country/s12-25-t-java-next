336b4178e5d0aa0d5cc866efb521066d17d159e8
Raul1978x <108808330+Raul1978x@users.noreply.github.com>
2023-12-12 13:25:56 -0300

    feat: security & cors, fix: endpoint base

---
+++ Backend/src/main/java/com/AlaCartApp/config/SecurityWeb.java
@@ -0,0 +1,45 @@
+package com.AlaCartApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+@Configuration
+@EnableWebSecurity
+public class SecurityWeb {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//        http.csrf(AbstractHttpConfigurer::disable)
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll();
+//                .requestMatchers("api").permitAll()
+             /*   .and()*//*.formLogin(
+                        form-> form
+                                .loginPage("/login")
+                                .permitAll()
+                                .successForwardUrl("/login/redilogin") // En caso de una autenticacion correcta redirige a "/thymeleaf/redilogin" que esta en LoginController
+                )*//*.logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .logoutSuccessUrl("/login") // A donde redirige cuando cierro sesion
+                                .invalidateHttpSession(true)
+                                .deleteCookies("JSESSIONID")
+                                .permitAll()
+                ).exceptionHandling();*/
+//                .accessDeniedPage("/login/accesoD");
+        return http.build();
+    }
+
+}
