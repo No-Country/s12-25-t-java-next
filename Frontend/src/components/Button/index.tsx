1558f56f876c3381e2b47c89401a3dd817e00522
arbelaism <arbelaism@outlook.com>
2023-12-11 21:22:06 -0300

    feat: add outline variant

--- Frontend/src/components/Button/index.tsx
+++ Frontend/src/components/Button/index.tsx
@@ -1,26 +1,29 @@
-import clsx from "clsx";
+import clsx from 'clsx'
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  text: string;
-  variant: "primary" | "secondary";
+  text: string
+  variant: 'primary' | 'secondary' | 'outline'
+  cn?: string
 }
 
 const BUTTON_VARIANTS = {
-  primary: "bg-primary-100 text-white",
-  secondary: "bg-secondary-100 text-white",
-};
+  primary: 'bg-primary-100 text-white',
+  secondary: 'bg-secondary-100 text-white',
+  outline: 'bg-transparent text-primary-100 border border-primary-100',
+}
 
-export default function Button({ text, variant, ...props }: Props) {
+export default function Button({ text, variant, cn, ...props }: Props) {
   return (
     <button
-      type="button"
+      type='button'
       className={clsx(
         BUTTON_VARIANTS[variant],
-        "w-44 h-10 px-4 py-2 md:py-5 md:px-6 rounded-3xl shadow-2xl font-medium",
+        cn,
+        'w-44 h-10 px-4 py-2 md:py-5 md:px-6 rounded-3xl shadow-2xl font-medium',
       )}
       {...props}
     >
       {text}
     </button>
-  );
+  )
 }
