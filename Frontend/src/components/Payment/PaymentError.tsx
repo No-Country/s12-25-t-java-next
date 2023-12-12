99694d0679ab91acb2b9c115b5ae3d5f49580b6a
arbelaism <arbelaism@outlook.com>
2023-12-11 21:22:34 -0300

    feat: finish payment error styling

--- Frontend/src/components/Payment/PaymentError.tsx
+++ Frontend/src/components/Payment/PaymentError.tsx
@@ -6,11 +6,14 @@ function PaymentError() {
     <div className='w-full h-full py-10 px-4 flex flex-col items-center justify-between gap-2'>
       <div className='text-center flex flex-col my-8 items-center justify-center gap-4'>
         <CardErrorSvg />
-        <h1 className='text-2xl font-semibold my-6'>
-          Ha ocurrido un error con tu pago
+        <h1 className='text-2xl font-semibold my-6 w-4/5'>
+          Ha ocurrido un error en tu pago
         </h1>
       </div>
-      <Button variant='primary' text='Reintentar pago' cn='w-full' />
+      <div className='w-full flex flex-col gap-4 items-center justify-center'>
+        <Button variant='primary' text='Reintentar pago' cn='w-full' />
+        <Button variant='outline' text='Llamar al mesero' cn='w-full' />
+      </div>
     </div>
   )
 }
