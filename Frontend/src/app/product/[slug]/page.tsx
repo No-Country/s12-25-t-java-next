d7a1ab77b9461c0bd75c832e9e6f5a56e30acbee
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-12-13 12:04:37 -0400

    created NoProducts component an logic

--- Frontend/src/app/product/[slug]/page.tsx
+++ Frontend/src/app/product/[slug]/page.tsx
@@ -1,21 +1,19 @@
-import ProductDescription from "@/components/ProductDescription";
-import React from "react";
-import { getProductById } from "@/lib/Products";
-import { Metadata } from "next";
+import ProductDescription from '@/components/ProductDescription'
+import React from 'react'
+import { getProductById } from '@/lib/Products'
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Producto",
-};
-
+	title: 'Producto',
+}
 
 const ProductPage = async ({ params }: { params: { slug: string } }) => {
-  const product = await getProductById(params.slug);
-  console.log(product);
-  return (
-    <div>
-      <ProductDescription product={product} />
-    </div>
-  );
-};
+	const product = await getProductById(params.slug)
+	return (
+		<div>
+			<ProductDescription product={product} />
+		</div>
+	)
+}
 
-export default ProductPage;
+export default ProductPage
