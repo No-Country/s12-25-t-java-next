b906cf847f74b24cdff453c9ee9dae5246d322ac
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-12-15 02:39:07 -0400

    fixed localstorage error

--- Frontend/src/app/admin/page.tsx
+++ Frontend/src/app/admin/page.tsx
@@ -4,7 +4,8 @@ import { redirect } from 'next/navigation'
 function page() {
 	// localStorage.setItem('user', '{"name":"usuario", "token":"123456789"}')
 
-	const user = localStorage.getItem('user')
+	// const user = localStorage.getItem('user')
+	const user = ''
 	!user ? redirect('admin/auth/login') : redirect('admin/dashboard/menu')
 }
 export default page
