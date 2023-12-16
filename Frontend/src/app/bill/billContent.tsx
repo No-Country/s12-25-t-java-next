b09d329bc511f145350d6066572c3c90a5173fff
Ever Jose Rojas perez <everjosejr@hotmail.com>
2023-12-15 20:46:25 -0500

    add- get order that the user does

--- Frontend/src/app/bill/billContent.tsx
+++ Frontend/src/app/bill/billContent.tsx
@@ -1,24 +1,22 @@
+import { OrderDetail } from "@/types/order";
 
-export default function BillContent({product,total}:{total:number}){
-    return(
-        <div className='px-5 mt-5  '> 
-    <div className="px-5 mt-5 border-b-2 border-[#E8E8E8]">
-             
-            <h1 className="text-black h-4 text-base font-normal font-sans my-2  ">
-              {product.product.name} 
-            </h1>
-           
-            <div className="flex flex-row justify-between">
-            <span className="text-lg font-semibold font-sans mt-[6px] ">
-              ${product.product.price} 
-            </span>
-            <span className="text-lg font-semibold font-sans mt-[6px] ">
-              {product.quantity} 
-            </span>
-            </div>
-           
-          </div>
-        
+export default function BillContent({ product}: {product:OrderDetail }) {
+  return (
+    <div className="px-5 mt-5  ">
+      <div className="px-5 mt-5 border-b-2 border-[#E8E8E8]">
+        <h1 className="text-black h-4 text-base font-normal font-sans my-2  ">
+          {product.product.name}
+        </h1>
+
+        <div className="flex flex-row justify-between">
+          <span className="text-lg font-semibold font-sans mt-[6px] ">
+            ${product.product.price}
+          </span>
+          <span className="text-lg font-semibold font-sans mt-[6px] ">
+            {product.quantity}
+          </span>
+        </div>
       </div>
-    )
+    </div>
+  );
 }
--- Frontend/src/app/bill/page.tsx
+++ Frontend/src/app/bill/page.tsx
@@ -1,28 +1,26 @@
 
 import HeaderBack from '@/components/Header/HeaderBack'
 import BillContent from './billContent'
+import { getOrder} from '@/lib/Orders'
 
-async function getOrders(){
-  const res = await fetch("https://657c7288853beeefdb997673.mockapi.io/api/v1/orders/6")
-  return res.json()
-}
- export default async function BillPages() {
-  const orders = await getOrders()
-  console.log(orders.detail)
+
+
+ export default async function BillPages({searchParams}:{searchParams: {order:number}}) {
+  
+  const orders = await getOrder(searchParams.order)
  
-
 
   return (
     <section className='bg-white min-h-[90vh] relative'>
       <HeaderBack text='Factura ' />
-      { orders.detail.map((product) => ( 
-       <BillContent key={product.id} product={product} />
+      { orders?.detail.map((product:any,index:number) => ( 
+       <BillContent key={index} product={product} />
        ))} 
        <div className="text-end px-5 mt-2">
           <h2 className="h-6 text-zinc-900 text-base font-medium font-sans">
             Subtotal
           </h2>
-           <span className="text-[22px] font-medium font-sans">{orders.total}</span> 
+           <span className="text-[22px] font-medium font-sans">{orders?.total}</span> 
         </div> 
       <div className='px-4 mt-2 absolute bottom-0 w-full'>
         <button
