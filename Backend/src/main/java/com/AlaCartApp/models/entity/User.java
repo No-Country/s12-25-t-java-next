b9e3a92bfdb3ca76d0206adf0e61865e8287c0b5
Christian Cachero <Ch_canchero@live.com.ar>
2023-12-18 00:27:32 -0300

    fix: se soluciono un bug que causaba que no se pudiera crear una mesa sin Id a pesar de que este argumento no fuera necesario.

--- Backend/src/main/java/com/AlaCartApp/models/entity/User.java
+++ Backend/src/main/java/com/AlaCartApp/models/entity/User.java
@@ -5,8 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 
 import java.util.Date;
--- Backend/src/main/java/com/AlaCartApp/repository/TableRepository.java
+++ Backend/src/main/java/com/AlaCartApp/repository/TableRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TableRepository extends JpaRepository<TableEntity, Long> {
+
+    TableEntity findByNumber(Integer number);
 }
