6985e40288ffdb00d09ec4fdd4a2ffa58e761e4f
arbelaism <arbelaism@outlook.com>
2023-12-17 23:37:51 -0300

    chore: add istock as known remote url

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,27 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'bucket-prueba-dif.s3.amazonaws.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'fakeimg.pl',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'images.unsplash.com',
-				port: '',
-				pathname: '/**',
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bucket-prueba-dif.s3.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fakeimg.pl',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
