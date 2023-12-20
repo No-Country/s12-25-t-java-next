8f59a9743ea7ea626d618614666316832288a085
YsisC <ysisclongart@gmail.com>
2023-12-20 12:37:31 -0300

    fix resultado and add trimto3word to  productname

--- Frontend/src/utils/currency.ts
+++ Frontend/src/utils/currency.ts
@@ -13,3 +13,19 @@ export const format = (value: number) => {
 export const addDecimals = (num: number) => {
   return (Math.round(num * 100) / 100).toFixed(2)
 }
+
+export const trimTo3Words = (text: string) => {
+  // Split the text into words
+  const words = text.split(' ');
+  const first3Words = words.slice(0, 3);
+
+  // Take only the first 3 words
+  if (words.length < 3) {
+    const remainingWords = words.slice(3);
+    first3Words.push(...remainingWords);
+  }
+  // Join the words back into a string
+  const result = first3Words.join(' ');
+
+  return result;
+}
